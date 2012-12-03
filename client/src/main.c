aa20d6afd9e814e83c03ee960ac8a702da0060ba
Jari Vetoniemi <mailroxas@gmail.com>
2012-12-03 23:01:18 +0200

    work

--- client/src/main.c
+++ client/src/main.c
@@ -3,9 +3,56 @@
 #include <assert.h>
 #include <enet/enet.h>
 #include <GL/glfw3.h>
-#include <GL/glhck.h>
+#include <glhck/glhck.h>
 
 #include "types.h"
+
+enum {
+   CAMERA_NONE       = 0,
+   CAMERA_UP         = 1,
+   CAMERA_DOWN       = 2,
+   CAMERA_LEFT       = 4,
+   CAMERA_RIGHT      = 8,
+   CAMERA_TURN_LEFT  = 16,
+   CAMERA_TURN_RIGHT = 32,
+   CAMERA_SLIDE      = 64,
+};
+
+enum {
+   ACTOR_NONE        = 0,
+   ACTOR_FORWARD     = 1,
+   ACTOR_BACKWARD    = 2,
+   ACTOR_LEFT        = 4,
+   ACTOR_RIGHT       = 8,
+};
+
+typedef struct GameActor {
+   glhckObject *object;
+   kmVec3 rotation;
+   kmVec3 position;
+   float speed;
+   unsigned int flags, lastFlags;
+} GameActor;
+
+typedef struct GameCamera {
+   glhckCamera *object;
+   kmVec3 rotation;
+   kmVec3 addRotation;
+   kmVec3 position;
+   kmVec3 offset;
+   float radius;
+   float speed;
+   unsigned int flags, lastFlags;
+} GameCamera;
+
+kmVec3* kmVec3Interpolate(kmVec3* pOut, const kmVec3* pIn, const kmVec3* other, float d)
+{
+   const float inv = 1.0f - d;
+   pOut->x = other->x*inv + pIn->x*d;
+   pOut->y = other->y*inv + pIn->y*d;
+   pOut->z = other->z*inv + pIn->z*d;
+   return pOut;
+}
 
 static int RUNNING = 0;
 static int WIDTH = 800, HEIGHT = 480;
@@ -149,6 +196,106 @@ static int manage_enet(client_data *data)
    return RETURN_OK;
 }
 
+int gameActorFlagsIsMoving(unsigned int flags)
+{
+   return (flags & ACTOR_FORWARD || flags & ACTOR_BACKWARD);
+}
+
+void gameCameraUpdate(GameCamera *camera, GameActor *target)
+{
+   float speed = camera->speed * 1.0f; /* multiply by interpolation */
+   kmVec3Assign(&camera->position, &target->position);
+   camera->rotation.z = target->rotation.z;
+
+   if (camera->flags & CAMERA_UP) {
+      camera->rotation.x += speed;
+      camera->flags &= ~CAMERA_SLIDE;
+   }
+   if (camera->flags & CAMERA_DOWN) {
+      camera->rotation.x -= speed;
+      camera->flags &= ~CAMERA_SLIDE;
+   }
+
+   if (camera->flags & CAMERA_RIGHT) {
+      camera->rotation.y -= speed;
+   }
+   if (camera->flags & CAMERA_LEFT) {
+      camera->rotation.y += speed;
+   }
+
+   if (!gameActorFlagsIsMoving(target->flags)) {
+      if (camera->flags & CAMERA_TURN_RIGHT) {
+         camera->rotation.y -= speed;
+      }
+      if (camera->flags & CAMERA_TURN_LEFT) {
+         camera->rotation.y += speed;
+      }
+   } else {
+      if (camera->flags & CAMERA_TURN_RIGHT) {
+         camera->addRotation.y -= speed;
+         camera->flags &= ~CAMERA_SLIDE;
+      }
+
+      if (camera->flags & CAMERA_TURN_LEFT) {
+         camera->addRotation.y += speed;
+         camera->flags &= ~CAMERA_SLIDE;
+      }
+   }
+
+   if (camera->flags & CAMERA_SLIDE) {
+      kmVec3 targetRotation = {0.0f,0.0f,0.0f};
+      kmVec3Interpolate(&camera->addRotation, &camera->addRotation, &targetRotation, 0.08f);
+   }
+
+   camera->position.x += cos(kmDegreesToRadians(camera->addRotation.y + camera->rotation.y + 90)) * camera->radius;
+   camera->position.z -= sin(kmDegreesToRadians(camera->addRotation.y + camera->rotation.y + 90)) * camera->radius;
+   camera->position.y += camera->rotation.x;
+
+   if (camera->rotation.x > 30) camera->rotation.x = 30;
+   if (camera->rotation.x < 0) camera->rotation.x  = 0;
+
+
+   glhckObject *internalObject = glhckCameraGetObject(camera->object);
+   kmVec3 targetPosition = target->position;
+   kmVec3Add(&targetPosition, &targetPosition, &camera->offset);
+   glhckObjectTarget(internalObject, &targetPosition);
+   glhckObjectPosition(internalObject, &camera->position);
+}
+
+void gameActorUpdateFrom3rdPersonCamera(GameActor *actor, GameCamera *camera)
+{
+   float speed = actor->speed * 1.0f; /* multiply by interpolation */
+
+   if (gameActorFlagsIsMoving(actor->flags) != gameActorFlagsIsMoving(actor->lastFlags)) {
+      if (!gameActorFlagsIsMoving(actor->flags)) {
+         camera->flags |= CAMERA_SLIDE;
+      } else {
+         camera->rotation.y = camera->rotation.y + camera->addRotation.y;
+         camera->addRotation.y = 0.0f;
+         camera->flags &= ~CAMERA_SLIDE;
+      }
+   }
+
+   if (actor->flags & ACTOR_FORWARD) {
+      actor->position.x -= speed * cos(kmDegreesToRadians(camera->rotation.y + 90));
+      actor->position.z += speed * sin(kmDegreesToRadians(camera->rotation.y + 90));
+
+      kmVec3 targetRotation = {0.0f,camera->rotation.y,0.0f};
+      kmVec3Interpolate(&actor->rotation, &actor->rotation, &targetRotation, 0.18f);
+   }
+
+   if (actor->flags & ACTOR_BACKWARD) {
+      actor->position.x += speed * cos(kmDegreesToRadians(camera->rotation.y + 90));
+      actor->position.z -= speed * sin(kmDegreesToRadians(camera->rotation.y + 90));
+
+      kmVec3 targetRotation = {0.0f,camera->rotation.y + 180,0.0f};
+      kmVec3Interpolate(&actor->rotation, &actor->rotation, &targetRotation, 0.18f);
+   }
+
+   glhckObjectRotation(actor->object, &actor->rotation);
+   glhckObjectPosition(actor->object, &actor->position);
+}
+
 int main(int argc, char **argv)
 {
    /* global data */
@@ -159,8 +306,11 @@ int main(int argc, char **argv)
    if (!glfwInit())
       return EXIT_FAILURE;
 
-   if (!(window = glfwOpenWindow(WIDTH, HEIGHT, GLFW_WINDOWED, "srv.birth", NULL)))
+   if (!(window = glfwCreateWindow(WIDTH, HEIGHT, GLFW_WINDOWED, "srv.birth", NULL)))
       return EXIT_FAILURE;
+
+   glfwSwapInterval(1);
+   glfwMakeContextCurrent(window);
 
    if (!glhckInit(argc, argv))
       return EXIT_FAILURE;
@@ -168,17 +318,95 @@ int main(int argc, char **argv)
    if (!glhckDisplayCreate(WIDTH, HEIGHT, 0))
       return EXIT_FAILURE;
 
-   if (init_enet("localhost", 1234, &data) != RETURN_OK)
-      return EXIT_FAILURE;
+   // if (init_enet("localhost", 1234, &data) != RETURN_OK)
+   //    return EXIT_FAILURE;
+
+   GameCamera camera;
+   memset(&camera, 0, sizeof(GameCamera));
+   camera.object = glhckCameraNew();
+   camera.radius = 30;
+   camera.speed  = 3;
+   camera.rotation.x = 10.0f;
+   kmVec3Fill(&camera.offset, 0.0f, 5.0f, 0.0f);
+   glhckCameraRange(camera.object, 1.0f, 500.0f);
+
+   GameActor player;
+   memset(&player, 0, sizeof(GameActor));
+   player.object = glhckCubeNew(1);
+   player.speed  = 1;
+   glhckObjectColorb(player.object, 255, 0, 0, 255);
+
+   glhckObject *ground = glhckPlaneNew(100);
+   glhckObjectRotatef(ground, 90.0f, 0.0f, 0.0f);
+   glhckObjectPositionf(ground, 0.0f, -1.0f, 0.0f);
+
+   unsigned int i, r, c = 1521;
+   glhckObject *cubes[c];
+   for (i = 0, r = 0; i != c; ++i) {
+      cubes[i] = glhckCubeNew(1);
+      glhckObjectColorb(cubes[i], 0, 0, 255, 255);
+      glhckObjectPositionf(cubes[i], 5.0f * (i % 39) - 95.0f, -1.0f, 5.0f * r - 95.0f);
+      r += ((i+1) % 39 == 0);
+   }
 
    RUNNING = 1;
    while (RUNNING && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS) {
-      manage_enet(&data);
-      glfwSwapBuffers();
+      // manage_enet(&data);
+      glfwPollEvents();
+
+      camera.lastFlags = camera.flags;
+      camera.flags = CAMERA_NONE;
+      if (camera.lastFlags & CAMERA_SLIDE) camera.flags |= CAMERA_SLIDE;
+
+      if (glfwGetKey(window, GLFW_KEY_UP)) {
+         camera.flags |= CAMERA_UP;
+      }
+      if (glfwGetKey(window, GLFW_KEY_DOWN)) {
+         camera.flags |= CAMERA_DOWN;
+      }
+      if (glfwGetKey(window, GLFW_KEY_RIGHT)) {
+         camera.flags |= CAMERA_TURN_RIGHT;
+      }
+      if (glfwGetKey(window, GLFW_KEY_LEFT)) {
+         camera.flags |= CAMERA_TURN_LEFT;
+      }
+      if (glfwGetKey(window, GLFW_KEY_E)) {
+         camera.flags |= CAMERA_SLIDE;
+      }
+      if (glfwGetKey(window, GLFW_KEY_D)) {
+         camera.flags |= CAMERA_RIGHT;
+      }
+      if (glfwGetKey(window, GLFW_KEY_A)) {
+         camera.flags |= CAMERA_LEFT;
+      }
+
+      player.lastFlags = player.flags;
+      player.flags = ACTOR_NONE;
+
+      if (glfwGetKey(window, GLFW_KEY_W)) {
+         player.flags |= ACTOR_FORWARD;
+      }
+      if (glfwGetKey(window, GLFW_KEY_S)) {
+         player.flags |= ACTOR_BACKWARD;
+      }
+
+      gameCameraUpdate(&camera, &player);
+      gameActorUpdateFrom3rdPersonCamera(&player, &camera);
+
+      glhckCameraUpdate(camera.object);
+      glhckObjectDraw(ground);
+      glhckObjectDraw(player.object);
+
+      for (i = 0; i != c; ++i) {
+         glhckObjectDraw(cubes[i]);
+      }
+
+      glhckRender();
+      glfwSwapBuffers(window);
       glhckClear();
    }
 
-   deinit_enet(&data);
+   // deinit_enet(&data);
    glhckTerminate();
    glfwTerminate();
    return EXIT_SUCCESS;
